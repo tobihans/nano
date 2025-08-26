82751d1f5d901342f79111643ed8105a6a8c6518
Hans B. K. Tognon <git@tobihans.space>
2025-08-27 00:39:50 +0100

    feat: start using typed settings for env vars

---
+++ main/environ.py
@@ -0,0 +1,13 @@
+from django.core.management.utils import get_random_secret_key
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Environ(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    debug: bool = False
+    secret_key: str = get_random_secret_key()
+    language_code: str = "en-us"
+
+
+env = Environ()
