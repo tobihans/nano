921864d649fef8d4aeb01f851036c387a908fc61
Hans B. K. Tognon <git@tobihans.space>
2025-08-28 00:15:28 +0100

    feat: create user migration

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,7 +2,7 @@
 See https://testdriven.io/blog/django-custom-user-model/
 """
 
-from django.contrib.auth.models import AbstractBaseUser, AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -10,6 +10,7 @@ from django.utils.translation import gettext_lazy as _
 from .managers import UserManager
 
 
+# NOTE: See https://testdriven.io/blog/django-custom-user-model/
 class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
@@ -25,3 +26,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
