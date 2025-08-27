16c87287a21b7276e8f7d7c20c7246e76cb3620d
Hans B. K. Tognon <git@tobihans.space>
2025-08-27 23:49:56 +0100

    wip: add custom user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,2 +1,27 @@
+"""
+See https://testdriven.io/blog/django-custom-user-model/
+"""
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(_("staff status"), default=False)
+    is_active = models.BooleanField(_("active"), default=True)
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = list(env.allowed_hosts)
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -121,3 +120,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
