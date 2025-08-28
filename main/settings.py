d78a3ed221c64f93ba44163834375a18f6cba795
Hans B. K. Tognon <git@tobihans.space>
2025-08-28 18:21:13 +0100

    feat: add tailwindcss

--- main/settings.py
+++ main/settings.py
@@ -139,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [BASE_DIR / "ui" / "dist"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
