8dcad00f9b1136cf11bfade20f8fa1a2f4205482
Hans B. K. Tognon <git@tobihans.space>
2025-08-28 18:21:48 +0100

    chore(dx): add tools for processes management

--- main/settings.py
+++ main/settings.py
@@ -140,7 +140,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [BASE_DIR / "ui" / "dist"]
+STATICFILES_DIRS = [
+    BASE_DIR / "ui" / "dist",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
