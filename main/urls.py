a2355f99a2d8248d3acc9af37fac6cdc99be9aca
Hans B. K. Tognon <git@tobihans.space>
2025-08-28 17:21:55 +0100

    wip: establish baseline for ui

--- main/urls.py
+++ main/urls.py
@@ -20,6 +20,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    *debug_toolbar_urls(),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-] + debug_toolbar_urls()
+    path("", include("apps.pages.urls")),
+]
