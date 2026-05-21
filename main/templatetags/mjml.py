f3eba5ba44da2a5e6e78baaaf31b659672f89dd1
Hans B. K. Tognon <git@tobihans.space>
2026-05-21 14:43:54 +0100

    chore: savepoint

---
+++ main/templatetags/mjml.py
@@ -0,0 +1,37 @@
+import mrml
+from django import template
+
+register = template.Library()
+
+
+class MjmlNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        content = self.nodelist.render(context)
+        return mrml.to_html(content)
+
+
+@register.tag("mjml")
+def do_mjml(parser, token):
+    """
+    Compile MJML to HTML after Django template rendering.
+
+    Usage:
+        {% load mjml %}
+        {% mjml %}
+        <mjml>
+          <mj-body>
+            <mj-section>
+              <mj-column>
+                <mj-text>Hello {{ user.name }}!</mj-text>
+              </mj-column>
+            </mj-section>
+          </mj-body>
+        </mjml>
+        {% endmjml %}
+    """
+    nodelist = parser.parse(("endmjml",))
+    parser.delete_first_token()
+    return MjmlNode(nodelist)
